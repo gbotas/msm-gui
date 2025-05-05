b6527fa6854f3a046cc5a0639f2e43e7c0781177
gbotas <gbotas@uchicago.edu>
2025-05-05 20:53:15 +0000

    commit with almost all forms for directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,4 +1,64 @@
 class DirectorsController < ApplicationController
+
+
+  def create
+    #Retrieve the user's inputs from params
+    #Create a record in the movie table
+    # Populate each column with the user input
+    # Save
+    # Redirect the user back to the /movies URL
+  
+    dir = Director.new
+  
+    dir.name = params.fetch("name_box")
+    dir.dob = params.fetch("dob_box")
+    dir.bio = params.fetch("bio_box")
+    dir.image = params.fetch("image_box")
+    
+    dir.save
+  
+    redirect_to("/directors")
+  
+  
+    end 
+  
+    def destroy
+      the_id = params.fetch("an_id")
+  
+      matching_director= Director.where({:id => the_id})
+  
+      the_director = matching_director.at(0)
+  
+      the_director.destroy
+      
+      redirect_to("/directors")
+  
+    end 
+  
+    def edit
+      #Get the ID out of params
+      m_id = params.fetch("the_id")
+  
+      # Look up existing record
+      matching_records = Movie.where({ :id => m_id})
+      the_movie = matching_records.at(0)
+  
+      # Overwrite each columns with user inputs
+      
+      the_movie.title = params.fetch("title_box")
+      the_movie.year = params.fetch("year_box")
+      the_movie.duration = params.fetch("duration_box")
+      the_movie.description = params.fetch("description_box") 
+      the_movie.image = params.fetch("image_box")
+      the_movie.director_id = params.fetch("director_id_box")
+  
+      #Save
+      the_movie.save
+      #Redirect to movie details page
+  
+      redirect_to("/movies/#{the_movie.id}")
+    end 
+  
   def index
     matching_directors = Director.all
     @list_of_directors = matching_directors.order({ :created_at => :desc })
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,7 @@
 class MoviesController < ApplicationController
 
+
+
   def create
   #Retrieve the user's inputs from params
   #Create a record in the movie table
@@ -34,6 +36,30 @@ class MoviesController < ApplicationController
     
     redirect_to("/movies")
 
+  end 
+
+  def edit
+    #Get the ID out of params
+    m_id = params.fetch("the_id")
+
+    # Look up existing record
+    matching_records = Movie.where({ :id => m_id})
+    the_movie = matching_records.at(0)
+
+    # Overwrite each columns with user inputs
+    
+    the_movie.title = params.fetch("title_box")
+    the_movie.year = params.fetch("year_box")
+    the_movie.duration = params.fetch("duration_box")
+    the_movie.description = params.fetch("description_box") 
+    the_movie.image = params.fetch("image_box")
+    the_movie.director_id = params.fetch("director_id_box")
+
+    #Save
+    the_movie.save
+    #Redirect to movie details page
+
+    redirect_to("/movies/#{the_movie.id}")
   end 
 
   def index
