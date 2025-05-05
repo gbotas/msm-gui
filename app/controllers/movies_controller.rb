3637347cbb6960b10bd8edb7df11392170b69c10
gbotas <gbotas@uchicago.edu>
2025-05-05 16:06:16 +0000

    firstcommit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,28 @@
 class MoviesController < ApplicationController
+
+  def create
+  #Retrieve the user's inputs from params
+  #Create a record in the movie table
+  # Populate each column with the user input
+  # Save
+  # Redirect the user back to the /movies URL
+
+  m = Movie.new
+
+  m.title = params.fetch("title_box")
+  m.year = params.fetch("year_box")
+  m.duration = params.fetch("duration_box")
+  m.description = params.fetch("description_box") 
+  m.image = params.fetch("image_box")
+  m.director_id = params.fetch("director_id_box")
+
+  m.save
+
+  redirect_to("/movies")
+
+
+  end 
+
   def index
     matching_movies = Movie.all
     @list_of_movies = matching_movies.order({ :created_at => :desc })
